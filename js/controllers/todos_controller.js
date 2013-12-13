2055f026b073f6c7c3cceffdd1b04d67aa285fb6
Scott Vossen <scottvossen@gmail.com>
2013-12-13 15:45:47 -0600

    Add clear completed

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,16 +1,24 @@
 TodoApp.TodosController = Ember.ArrayController.extend({
 
-  remaining: function () {
+  remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
 
-  inflection: function () {
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
   }.property('remaining'),
 
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
   actions: {
-    createTodo: function () {
+    createTodo: function() {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
       if (!title.trim()) { return; }
@@ -26,6 +34,12 @@ TodoApp.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
-    }
-  }
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
+  },
 });
