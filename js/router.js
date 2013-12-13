481bfedb54d25ee5a6ac7f1b9d14359e87918052
Scott Vossen <scottvossen@gmail.com>
2013-12-13 14:50:03 -0600

    Configure routing for child routes

--- js/router.js
+++ js/router.js
@@ -1,9 +1,17 @@
 TodoApp.Router.map(function() {
-	this.resource('todos', { path: '/'});
+	this.resource('todos', { path: '/' }, function() {
+		// child routes
+	});
 });
 
 TodoApp.TodosRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
+	}
+});
+
+TodoApp.TodosIndexRoute = Ember.Route.extend({
+	model: function() {
+		return this.modelFor('todos');
 	}
 });
